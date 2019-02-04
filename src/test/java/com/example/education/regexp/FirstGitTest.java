aa8137e4f25b5c09bcde08ca02b97e2307c98942
Dmytro Lykhosherst <d.lihosherst@gmail.com>
2019-02-04 20:35:00 +0100

    first 2

--- src/test/java/com/example/education/regexp/FirstGitTest.java
+++ src/test/java/com/example/education/regexp/FirstGitTest.java
@@ -3,4 +3,12 @@ package com.example.education.regexp;
 public class FirstGitTest {
 
     public String a = "45";
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
 }
